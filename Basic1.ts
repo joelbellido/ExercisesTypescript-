8261a74843fb639b10a7843d85483e4ad2dd8798
EPINTADO <epintado18@gmail.com>
2019-07-25 19:38:56 -0500

    first

---
+++ Basic1.ts
@@ -0,0 +1,42 @@
+
+
+let  a='hola';
+a='hjolas';
+
+
+let b: number =10;
+
+
+b=20;
+b =10+10;
+
+const num1=10;
+const num2=20;
+b= num1+num2;
+
+
+function  suma(num1:number ,num2:number ):number {
+    return num1 + num2;
+}
+let anyValue: any =10;
+
+anyValue = 'hola';
+
+suma (1,2);
+
+function sumaJavaScriptVanilla(num1,num2) {
+    return num1+num2;
+}
+
+
+type dni = number;
+
+let dniNumber :dni =2123
+
+let dniNumber2 :dni =2123;
+
+
+
+
+
+
