e19be99d974099113adc2e8e33776be2b17e4406
EPINTADO <epintado18@gmail.com>
2019-07-25 23:05:33 -0500

    Decorators: aplicación en clases

--- Basic8.ts
+++ Basic8.ts
@@ -1,20 +1,20 @@
 
 
-function log(target , key) {
-    console.log(key+'se ah llamado');
-}
-class Persona{
-    private  name:string;
-
-    constructor(name:string){
-        this.name=name;
-    }
-    @log
-    sayMyName(){
-        console.log(this.name);
-    }
-
-}
-
-const persona:Persona =new Persona('alan');
-persona.sayMyName();//alan
+// function log(target , key) {
+//     console.log(key+'se ah llamado');
+// }
+// class Persona{
+//     private  name:string;
+//
+//     constructor(name:string){
+//         this.name=name;
+//     }
+//     // @log
+//     // sayMyName(){
+//     //     console.log(this.name);
+//     // }
+//
+// }
+//
+// const persona:Persona =new Persona('alan');
+// persona.sayMyName();//alan
