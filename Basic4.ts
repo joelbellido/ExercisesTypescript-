d954501619854980ac74698fac359753959fa83c
EPINTADO <epintado18@gmail.com>
2019-07-25 21:51:31 -0500

    shapes

---
+++ Basic4.ts
@@ -0,0 +1,30 @@
+class Persona {
+
+
+    private edad:number;
+    private altura:number;
+    private dni:string;
+
+    constructor (edad:number , altura:number , dni:string){
+        this.edad=edad;
+        this.altura=altura;
+        this.dni=dni;
+    }
+
+}
+
+class Alumno extends Persona{
+
+    private matricula:string;
+
+    constructor (edad:number , altura:number , dni:string , matricula:string){
+        super(edad,altura,dni);
+        this.matricula=matricula;
+    }
+}
+
+let persona:Persona = new Persona(27,1.69,'326598');
+
+let alumno:Alumno =new Alumno(27,1.69,'36521','12356');
+persona=alumno;
+alumno = persona;
